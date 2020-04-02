85f02476baa904d94df502c494ed3581483f3452
Salvador <salvadjg@uci.edu>
2020-04-02 16:23:51 -0700

    Update Agent.java

--- src/Agent.java
+++ src/Agent.java
@@ -1,25 +1,3 @@
-// ======================================================================
-// FILE:        Agent.java
-//
-// AUTHOR:      Abdullah Younis
-//
-// DESCRIPTION: This file contains the abstract agent class, which
-//              details the interface for a wumpus agent. The actuators
-//              are listed in the 'Action' enum, while the sensors are
-//              parameters to the abstract function 'getAction'. Any
-//              agent will need to implement the getAction function,
-//              which returns an Action for every turn in the game.
-//
-// NOTES:       - An agent is anything that can be viewed as perceiving
-//                its environment through sensors and acting upon that
-//                environment through actuators
-//
-//              - Throughout this project Agent and AI are
-//                interchangeable
-//
-//              - Don't make changes to this file.
-// ======================================================================
-
 abstract class Agent
 {
 	
